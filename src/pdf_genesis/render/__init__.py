235417dcbf4d415e63588405980046a9100ea6c6
jrb00013 <jrb00013wvu@gmail.com>
2026-06-01 02:16:43 -0400

    Export render package public API.

---
+++ src/pdf_genesis/render/__init__.py
@@ -0,0 +1,11 @@
+from pdf_genesis.render.bench import render_bench_pdf
+from pdf_genesis.render.chorus import render_chorus_pdf
+from pdf_genesis.render.design import render_design_pdf
+from pdf_genesis.render.patent import render_patent_pdf
+
+__all__ = [
+    "render_chorus_pdf",
+    "render_design_pdf",
+    "render_patent_pdf",
+    "render_bench_pdf",
+]
